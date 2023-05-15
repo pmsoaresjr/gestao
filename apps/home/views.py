a445786fed2cdf769956a5cf4ffb65163bc1850d
paulo-marcio <paulo.soares@resale.com.br>
2023-05-15 18:34:54 -0300

    feat add viewss

--- apps/home/views.py
+++ apps/home/views.py
@@ -8,6 +8,9 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from django.urls import reverse
+from .models import Cliente
+from django.views.generic import ListView
+
 
 
 @login_required(login_url="/login/")
@@ -26,12 +29,15 @@ def pages(request):
     try:
 
         load_template = request.path.split('/')[-1]
+        context["clientes"] = Cliente.objects.all()
+
 
         if load_template == 'admin':
             return HttpResponseRedirect(reverse('admin:index'))
         context['segment'] = load_template
 
         html_template = loader.get_template('home/' + load_template)
+        
         return HttpResponse(html_template.render(context, request))
 
     except template.TemplateDoesNotExist:
@@ -42,3 +48,20 @@ def pages(request):
     except:
         html_template = loader.get_template('home/page-500.html')
         return HttpResponse(html_template.render(context, request))
+
+
+
+class BookListView(ListView):
+    model = Cliente
+
+    def head(self, *args, **kwargs):
+        last_book = self.get_queryset().latest("data_criacao")
+        response = HttpResponse(
+            # RFC 1123 date format.
+            headers={
+                "Last-Modified": last_book.publication_date.strftime(
+                    "%a, %d %b %Y %H:%M:%S GMT"
+                )
+            },
+        )
+        return response
